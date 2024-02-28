40e318319f64e04de27a389acc1d31fa17a250a0
abhijeet <abhijeetkoli39@gamil.com>
2024-02-28 17:42:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import {Switch, Route, Redirect} from 'react-router-dom'
+import LoginPage from './components/LoginPage'
+import HomeRoute from './components/Home'
+import MovieDetailView from './components/MovieDetailView'
+import Popular from './components/Popular'
+import Account from './components/Account'
+import Search from './components/Search'
+import ProfilePage from './components/ProfilePage'
+import ProtectedRoute from './components/ProtectedRoute'
+import NotFound from './components/NotFound'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <Switch>
+      <Route path="/login" component={LoginPage} />
+      <ProtectedRoute exact path="/" component={HomeRoute} />
+      <ProtectedRoute exact path="/movies/:id" component={MovieDetailView} />
+      <ProtectedRoute exact path="/search" component={Search} />
+      <ProtectedRoute exact path="/popular" component={Popular} />
+      <ProtectedRoute exact path="/account" component={Account} />
+      <ProtectedRoute exact path="/profilepage" component={ProfilePage} />
 
-export default App;
+      <Route path="/not-found" component={NotFound} />
+      <Redirect to="not-found" />
+    </Switch>
+  </>
+)
+
+export default App
+
+// ccbp submit RJSCP79ATS
